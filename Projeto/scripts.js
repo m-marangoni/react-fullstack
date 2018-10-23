efdd999c1c2a548fa936c6ba2ede980af570c67c
Mariana <marangoni.mariana@gmail.com>
2018-10-22 21:32:01 -0300

    pure js code-example1

--- Projeto/scripts.js
+++ Projeto/scripts.js
@@ -1,1 +1,20 @@
+//fazendo o link com js vanilla apenas
+
+const linkCriarUmaConta = document.createElement('a')
+linkCriarUmaConta.className = "link"
+linkCriarUmaConta.href = "/conta"
+linkCriarUmaConta.appendChild(document.createTextNode('Criar uma conta'))
+
+//fazendo link login com js vanilla apenas
+const linkFazerLogin = document.createElement('a')
+linkFazerLogin.className = "link"
+linkFazerLogin.href = "/login"
+linkFazerLogin.appendChild(document.createTextNode('Fazer login'))
+
+const formulario = document.createElement('form')
+formulario.appendChild(linkCriarUmaConta)
+formulario.appendChild(linkFazerLogin)
+
+const divisaoProjeto = document.getElementById("projeto")
+divisaoProjeto.appendChild(formulario)
 
